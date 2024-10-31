b0249bf9ec1e5a65b7359c134ec9b1158c5ba393
G S ashish <gsashishemail@gmail.com>
2024-11-01 01:40:52 +0530

    updated routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useEffect } from "react";
 import "./App.scss";
+import AppRoutes from "./routes";
 
 function App() {
   useEffect(() => {
     document.documentElement.classList.toggle("dark", false);
   }, []);
-  return <div className="">Hello</div>;
+  return <AppRoutes />;
 }
 
 export default App;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,23 +1,24 @@
 // src/App.jsx
 import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PublicLayout from "../layout/PublicLayout";
 
 // const Home = lazy(() => import("./pages/Home"));
 // const Login = lazy(() => import("./pages/Login"));
 // const Dashboard = lazy(() => import("./pages/Dashboard"));
 
-const App = () => {
+const AppRoutes = () => {
   return (
     <Router>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          {/* Public routes */}
           {/* <Route element={<PublicLayout />}>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
           </Route> */}
-
-          {/* Private routes */}
+          <Route element={<PublicLayout />}>
+            <Route path="/" element={() => <div>Hello page</div>} />
+          </Route>
           {/* <Route element={<PrivateLayout />}>
             <Route path="/dashboard" element={<Dashboard />} />
           </Route> */}
@@ -27,4 +28,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default AppRoutes;
