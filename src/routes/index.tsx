3c6c1e0820761ea47584e120e93dab74ace5292a
G S ashish <gsashishemail@gmail.com>
2024-11-01 12:44:38 +0530

    added signup page and login page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,8 +2,10 @@
 import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PublicLayout from "../layout/PublicLayout";
+import { ROUTES } from "../utils/routes";
 
-// const Home = lazy(() => import("./pages/Home"));
+const Login = lazy(() => import("../pages/public/LoginPage"));
+const Signup = lazy(() => import("../pages/public/SignUp"));
 // const Login = lazy(() => import("./pages/Login"));
 // const Dashboard = lazy(() => import("./pages/Dashboard"));
 
@@ -12,12 +14,9 @@ const AppRoutes = () => {
     <Router>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          {/* <Route element={<PublicLayout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-          </Route> */}
           <Route element={<PublicLayout />}>
-            <Route path="/" element={() => <div>Hello page</div>} />
+            <Route path={ROUTES.login} element={<Login />} />
+            <Route path={ROUTES.signup} element={<Signup />} />
           </Route>
           {/* <Route element={<PrivateLayout />}>
             <Route path="/dashboard" element={<Dashboard />} />
