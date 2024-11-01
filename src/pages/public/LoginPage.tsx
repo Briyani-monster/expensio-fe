e9ae13448583ffc2a46224c99d50ca6fe99a72e7
G S ashish <gsashishemail@gmail.com>
2024-11-01 15:28:16 +0530

    sign in and sign up page ui done

--- src/pages/public/LoginPage.tsx
+++ src/pages/public/LoginPage.tsx
@@ -1,7 +1,36 @@
 import React from "react";
-
+import "./style.scss";
+import { Link } from "react-router-dom";
 function LoginPage() {
-  return <div>LoginPage</div>;
+  return (
+    <div className="text-white flex flex-col gap-12 items-center justify-center h-full text-center">
+      <div>
+        <p>Hello! 👋</p>
+        <p>we are happy to see you again!</p>
+      </div>
+      <div className="flex flex-col gap-5">
+        <input type="text" className="input-style-glass " placeholder="Email" />
+        <div className="flex flex-col gap-1">
+          <input
+            type="text"
+            placeholder="Password"
+            className="input-style-glass "
+          />
+
+          <Link to="/forgot-password" className="underline text-right">
+            forgot password?
+          </Link>
+        </div>
+        <button className="bg-black rounded-sm mt-3 py-2 px-10">Login</button>
+        <div className="flex gap-1">
+          <p>want an account?</p>
+          <Link to="/signup" className="underline">
+            Sign up
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LoginPage;
--- src/pages/public/Signup.tsx
+++ src/pages/public/Signup.tsx
@@ -1,7 +1,41 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import "./style.scss";
 function Signup() {
-  return <div>Signup</div>;
+  return (
+    <div className="text-white flex flex-col gap-12 items-center justify-center h-full text-center">
+      <div>
+        <p>Welcome! 👋</p>
+        <p>We’re excited to have you join us!</p>
+        <p>Let’s get you started on your financial journey.</p>
+      </div>
+      <div className="flex flex-col gap-5">
+        <input
+          type="text"
+          className="input-style-glass "
+          placeholder="Full name"
+        />
+        <input type="text" className="input-style-glass " placeholder="Email" />
+        <input
+          type="text"
+          placeholder="Password"
+          className="input-style-glass "
+        />
+        <input
+          type="text"
+          placeholder="Confirm password"
+          className="input-style-glass "
+        />
+        <button className="bg-black rounded-sm mt-3 py-2 px-10">Sign up</button>
+        <div className="flex gap-1">
+          <p>Already have an account?</p>
+          <Link to="/login" className="underline">
+            Login
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Signup;
